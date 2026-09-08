966d7f0604e433feb05cabe8f876a0803222d853
Max1mus <perepichkamax10@gmail.com>
2026-09-08 17:43:13 +0300

    Configure GitHub Pages deployment

--- src/components/AppRoot.jsx
+++ src/components/AppRoot.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import Header from './Header'
 import Footer from './Footer'
@@ -9,39 +9,40 @@ import Contacts from '../pages/Contacts'
 import Roulette from '../pages/Roulette'
 
 function ScrollToTop() {
-    const { pathname, hash } = useLocation()
+  const { pathname, hash } = useLocation()
 
-    useEffect(() => {
-        if (hash) {
-            const target = document.getElementById(hash.replace('#', ''))
+  useEffect(() => {
+    if (hash) {
+      const target = document.getElementById(hash.replace('#', ''))
 
-            if (target) {
-                target.scrollIntoView({ behavior: 'smooth', block: 'start' })
-                return
-            }
-        }
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        return
+      }
+    }
 
-        window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
-    }, [pathname, hash])
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname, hash])
 
-    return null
+  return null
 }
 
 export default function AppRoot() {
-    return (
-        <BrowserRouter>
-            <ScrollToTop />
-            <div className="app-shell">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/tours" element={<Tours />} />
-                    <Route path="/contacts" element={<Contacts />} />
-                    <Route path="/roulette" element={<Roulette />} />
-                </Routes>
-                <Footer />
-            </div>
-        </BrowserRouter>
-    )
+  return (
+    <HashRouter basename="/Tourist-Agention">
+      <ScrollToTop />
+      <div className="app-shell">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/roulette" element={<Roulette />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </div>
+    </HashRouter>
+  )
 }
